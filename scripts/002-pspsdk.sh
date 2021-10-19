42748cf3040213dc0d20eed85ad4d78ea8b14f7b
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-10-19 19:37:22 +0200

    Creating symbolic links for the standard libraries

--- scripts/002-pspsdk.sh
+++ scripts/002-pspsdk.sh
@@ -23,3 +23,12 @@ make --quiet -j $PROC_NR clean          || { exit 1; }
 make --quiet -j $PROC_NR all            || { exit 1; }
 make --quiet -j $PROC_NR install        || { exit 1; }
 make --quiet -j $PROC_NR clean          || { exit 1; }
+
+## gcc needs to include libpsplibc libpsputility libpspnet_inet libpspnet_resolver libpspuser libpspkernel
+## from pspsdk to be able to build executables, because they are part of the standard libraries
+ln -sf "$PSPDEV/psp/sdk/lib/libpsplibc.a" "$PSPDEV/psp/lib/libpsplibc.a" || { exit 1; }
+ln -sf "$PSPDEV/psp/sdk/lib/libpsputility.a" "$PSPDEV/psp/lib/libpsputility.a" || { exit 1; }
+ln -sf "$PSPDEV/psp/sdk/lib/libpspnet_inet.a" "$PSPDEV/psp/lib/libpspnet_inet.a" || { exit 1; }
+ln -sf "$PSPDEV/psp/sdk/lib/libpspnet_resolver.a" "$PSPDEV/psp/lib/libpspnet_resolver.a" || { exit 1; }
+ln -sf "$PSPDEV/psp/sdk/lib/libpspuser.a" "$PSPDEV/psp/lib/libpspuser.a" || { exit 1; }
+ln -sf "$PSPDEV/psp/sdk/lib/libpspkernel.a" "$PSPDEV/psp/lib/libpspkernel.a" || { exit 1; }
